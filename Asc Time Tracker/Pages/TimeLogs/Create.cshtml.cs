e28f35a5c41fc8cbd2bc46757ccaff2e1b3b934c
pton@ascmt.com <pton@ascmt.com>
2021-06-29 14:07:08 -0700

    Working on modal for adding time logs

---
+++ Asc Time Tracker/Pages/TimeLogs/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Asc_Time_Tracker.Data;
+using Asc_Time_Tracker.Models;
+
+namespace Asc_Time_Tracker.Pages.TimeLogs
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Asc_Time_Tracker.Data.Asc_Time_TrackerContext _context;
+
+        public CreateModel(Asc_Time_Tracker.Data.Asc_Time_TrackerContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public TimeLog TimeLog { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.TimeLog.Add(TimeLog);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
