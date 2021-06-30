e282b490758d80d40646cca2feb013e204f9461c
pton@ascmt.com <pton@ascmt.com>
2021-06-30 11:49:20 -0700

    Checkbox tweaking and attempts to get submit working

--- Asc Time Tracker/wwwroot/js/createForm.js
+++
@@ -1,8 +0,0 @@
-﻿// Detect when our modal form is submitted and create the new time log.
-/*
-$('#timeLogFormSubmit').click(function (e) {
-    e.preventDefault();
-    $.post('TimeLogs/Create/OnPostAsync',
-        $('#timeLogForm').serialize());
-});
-*/
---
+++ Asc Time Tracker/wwwroot/js/submitModal.js
@@ -0,0 +1,12 @@
+﻿$('#timeLogForm').submit(function () {
+    $.ajax({
+        url: this.action,
+        type: this.method,
+        data: $(this).serialize(),
+        success: function (result) {
+            if (result.success) {
+                $('#exampleModal').modal('hide');
+            }
+        }
+    });
+})
