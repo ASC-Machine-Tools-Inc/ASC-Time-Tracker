0f6746fee483cca1433591e493260d267b00512a
pton@ascmt.com <pton@ascmt.com>
2021-07-06 09:14:53 -0700

    Added script for generating random color for job numbers

---
+++ Asc Time Tracker/wwwroot/js/ui/jobColor.js
@@ -0,0 +1,17 @@
+﻿// Creates a random color to denote a certain job number. If none, defaults to red.
+$('.log-color').each(function () {
+    let saturation = '85%';
+    let lightness = '50%';
+
+    // Get hashcode from job number
+    let jobNum = $(this).siblings('.log-job').html();
+    let hash = 0;
+    for (let i = 0; i < jobNum.length; i++) {
+        hash = jobNum.charCodeAt(i) + ((hash << 5) - hash);
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    let hue = hash % 360;
+
+    let hsl = 'hsl(' + hue + ',' + saturation + ',' + lightness + ')';
+    $(this).css('background-color', hsl);
+});
