5b20187e8d8df1027c39591bee9b895b5e2a94fd
Phi <pton@ascmt.com>
2021-08-10 10:26:35 -0700

    Working on brand new time log index

---
+++ Asc Time Tracker/Pages/TimeLogs/_IndexLogs.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Asc_Time_Tracker.Pages.TimeLogs
+{
+    public partial class IndexLogsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
