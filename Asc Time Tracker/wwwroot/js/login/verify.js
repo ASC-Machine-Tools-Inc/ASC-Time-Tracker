01979a7c1ca09a9a448224a83d62469b3feedee7
Phi <pton@ascmt.com>
2021-08-30 13:32:12 -0700

    Code cleanup

--- Asc Time Tracker/wwwroot/js/login/verify.js
+++
@@ -1,6 +0,0 @@
-﻿// Display a modal prompting the user for their employee ID and name if we
-// don't already have them in localstorage. Heinously insecure, but since
-// this is all internal it should be fine for now.
-
-function verifyUser() {
-}
