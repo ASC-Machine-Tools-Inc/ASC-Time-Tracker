f702fb9d9c8e683c02484541f11a36308e1e16d7
Phi <pton@ascmt.com>
2021-09-09 15:48:33 -0700

    Changed timer row to grid instead for better mobile appearance, working on site tour

---
+++ Asc Time Tracker/wwwroot/js/ui/introTour.js
@@ -0,0 +1,17 @@
+﻿// TODO: run this only on first login
+introJs().setOptions({
+    showProgress: true,
+    steps: [{
+        title: 'Welcome',
+        intro: 'Hello World!'
+    },
+    {
+        element: document.querySelector(".sidebar"),
+        intro: 'This <b>STEP</b> focuses on an image. <br/> We also used some HTML tags!'
+    },
+    {
+        title: 'Farewell!',
+        element: document.getElementById("addTimerBtn"),
+        intro: "Adios!"
+    }]
+}).start();
